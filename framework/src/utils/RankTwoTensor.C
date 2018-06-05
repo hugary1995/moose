631b6e3df86a111536d58f25f3c5f390fa242053
Tianchen Hu <hugary1995@gmail.com>
2018-06-05 09:50:52 -0600

    fix ranktwotensor

--- framework/src/utils/RankTwoTensor.C
+++ framework/src/utils/RankTwoTensor.C
@@ -480,6 +480,8 @@ RankTwoTensor::positveProjectionEigenDecomposition() const
   // C. Miehe and M. Lambrecht, Commun. Numer. Meth. Engng 2001; 17:337~353
 
   // Compute eigenvectors and eigenvalues of *this
+  RankTwoTensor eigvec;
+  std::vector<Real> eigval;
   (*this).symmetricEigenvaluesEigenvectors(eigval, eigvec);
 
   // Separate out positive and negative eigen values
