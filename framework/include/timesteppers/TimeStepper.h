07bee010e385c66f30b7a52106413063926bb3c7
Gary Hu <hugary1995@gmail.com>
2021-02-13 09:33:15 -0500

    raccoon customization of moose

--- framework/include/timesteppers/TimeStepper.h
+++ framework/include/timesteppers/TimeStepper.h
@@ -12,6 +12,7 @@
 #include "MooseObject.h"
 #include "Restartable.h"
 #include "ScalarCoupleable.h"
+#include "SolveObject.h"
 
 class TimeStepper;
 class FEProblemBase;
@@ -35,6 +36,9 @@ public:
    * Initialize the time stepper. Called at the very beginning of Executioner::execute()
    */
   virtual void init();
+
+  /// Set the solver
+  void setInnerSolve(SolveObject * solver) { _solver = solver; }
 
   virtual void preExecute();
   virtual void preSolve() {}
@@ -156,6 +160,9 @@ protected:
   bool _has_reset_dt;
 
 private:
+  /// The solver being wrapped
+  SolveObject * _solver;
+
   /// Size of the current time step as computed by the Stepper.  Note that the actual dt that was taken might be smaller if the Executioner constrained it.
   Real & _current_dt;
 };
