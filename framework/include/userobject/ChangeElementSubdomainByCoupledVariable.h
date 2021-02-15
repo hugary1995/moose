6aa8aede5cf1c343e4fbc4510d8030fe85ed11cf
Gary Hu <hugary1995@gmail.com>
2021-02-15 16:47:08 -0500

    add ChangeElementSubdomainUserObjects

---
+++ framework/include/userobject/ChangeElementSubdomainByCoupledVariable.h
@@ -0,0 +1,36 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ChangeElementSubdomainUserObjectBase.h"
+#include "MooseEnum.h"
+
+class ChangeElementSubdomainByCoupledVariable : public ChangeElementSubdomainUserObjectBase
+{
+public:
+  static InputParameters validParams();
+
+  ChangeElementSubdomainByCoupledVariable(const InputParameters & parameters);
+
+  virtual bool shouldChangeSubdomainFromOriginToTarget() override;
+
+  virtual bool shouldChangeSubdomainFromTargetToOrigin() override
+  {
+    return !shouldChangeSubdomainFromOriginToTarget();
+  }
+
+protected:
+  /// variable value to decide wether an element whould be activated
+  const VariableValue & _coupled_var;
+  /// variable value to decide wether an element whould be activated
+  const Real _activate_value;
+  /// type of activation - blow or above
+  const enum class ActivateType { BELOW, EQUAL, ABOVE } _activate_type;
+};
