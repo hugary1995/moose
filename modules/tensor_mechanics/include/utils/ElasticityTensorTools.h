51dd779627183cf9cd2d76b7a43e3bb179cadcd4
Gary Hu <hugary1995@gmail.com>
2021-03-24 17:04:54 -0400

    Refactor phase field fracture models as damage models
    Refactor the phase field fracture stress calculators by deriving from DamageBase.
    
    close #17620

--- modules/tensor_mechanics/include/utils/ElasticityTensorTools.h
+++ modules/tensor_mechanics/include/utils/ElasticityTensorTools.h
@@ -67,6 +67,17 @@ momentJacobianWC(const RankFourTensor & r4t, unsigned int i, unsigned int k, Rea
  */
 template <typename T>
 T
+getIsotropicLameConstant(const RankFourTensorTempl<T> & elasticity_tensor)
+{
+  return elasticity_tensor(0, 0, 1, 1);
+}
+
+/**
+ * Get the shear modulus for an isotropic elasticity tensor
+ * param elasticity_tensor the tensor (must be isotropic, but not checked for efficiency)
+ */
+template <typename T>
+T
 getIsotropicShearModulus(const RankFourTensorTempl<T> & elasticity_tensor)
 {
   return elasticity_tensor(0, 1, 0, 1);
